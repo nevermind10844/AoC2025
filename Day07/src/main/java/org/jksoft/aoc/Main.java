85246facc96059472b7b84398f336ab234447d6e
Jan <jan.kressin@googlemail.com>
2025-12-07 18:04:18 +0100

    Day 7 Part 1 result.txt shortened and scrambled

---
+++ Day07/src/main/java/org/jksoft/aoc/Main.java
@@ -0,0 +1,26 @@
+package org.jksoft.aoc;
+
+import org.jksoft.utils.grid.Grid;
+import org.jksoft.utils.grid.MappingConfiguration;
+import org.jksoft.utils.grid.TileType;
+import org.jksoft.utils.inputreader.InputReader;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		MappingConfiguration mappingConfig = new MappingConfiguration();
+		mappingConfig.addMapping('S', TileType.STAR);
+		mappingConfig.addMapping('^', TileType.PLUS);
+		mappingConfig.addMapping('|', TileType.LETTER_B);
+		Grid grid = Grid.parse(InputReader.readInput(), mappingConfig);
+		
+		TachyonLab lab = new TachyonLab(grid);
+		lab.fire();
+		
+		int result = lab.getResult();
+		System.out.println(result);
+	}
+	
+
+}
---
+++ Day07/src/main/java/org/jksoft/aoc/TachyonLab.java
@@ -0,0 +1,56 @@
+package org.jksoft.aoc;
+
+import org.jksoft.utils.grid.Grid;
+import org.jksoft.utils.grid.Tile;
+import org.jksoft.utils.grid.TileType;
+
+public class TachyonLab {
+	private Grid grid;
+	
+	private int splits;
+	
+	public TachyonLab(Grid grid) {
+		this.grid = grid;
+		this.splits = 0;
+	}
+	
+	public void fire() {
+		System.out.println("firing up tachyon ray emitter");
+		System.out.println("heating-sequnece succesfull");
+		System.out.println("aperture closed for a concentrated beam");
+		System.out.println("critical beam focus detected");
+		System.out.println("shutter opened");
+		Tile tile = this.grid.findTile(TileType.STAR);
+		followRay(tile);
+		System.out.println(this.grid);
+	}
+	
+	private void followRay(Tile current) {
+		if(current.getY() >= this.grid.getHeight() - 1) {
+			return;
+		}
+		
+		Tile below = this.grid.getSouthernNeighbor(current);
+		if(below.getType().equals(TileType.DOT)) {
+			below.setType(TileType.LETTER_B);
+			followRay(below);
+		} else if(below.getType().equals(TileType.PLUS)){
+			this.splits++;
+			Tile left = this.grid.getWesternNeighbor(below);
+			if(left.getType().equals(TileType.DOT)) {
+				left.setType(TileType.LETTER_B);
+				followRay(left);
+			}
+			Tile right = this.grid.getEasternNeighbor(below);
+			if(right.getType().equals(TileType.DOT)) {
+				right.setType(TileType.LETTER_B);
+				followRay(right);
+			}
+		}
+	}
+	
+	
+	public int getResult() {
+		return this.splits;
+	}
+}
