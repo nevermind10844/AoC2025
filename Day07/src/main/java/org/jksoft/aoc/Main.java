0e65f9b0201315a7cc6c58a2699444a81f85c81c
Jan <jan.kressin@googlemail.com>
2025-12-14 10:29:20 +0100

    Day 07 part 2, using new grid-system and DP

--- Day07/src/main/java/org/jksoft/aoc/Main.java
+++ Day07/src/main/java/org/jksoft/aoc/Main.java
@@ -1,5 +1,7 @@
 package org.jksoft.aoc;
 
+import java.util.List;
+
 import org.jksoft.utils.grid.Grid;
 import org.jksoft.utils.grid.MappingConfiguration;
 import org.jksoft.utils.grid.TileType;
@@ -8,18 +10,23 @@ import org.jksoft.utils.inputreader.InputReader;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		MappingConfiguration mappingConfig = new MappingConfiguration();
 		mappingConfig.addMapping('S', TileType.STAR);
 		mappingConfig.addMapping('^', TileType.PLUS);
-		Grid grid = Grid.parse(InputReader.readInput(), mappingConfig);
 		
-		TachyonLab lab = new TachyonLab(grid);
-		lab.fire();
+//		List<String> reduced = InputReader.readTestInput(2);
 		
-		int result = lab.getResult();
-		System.out.println(result);
+//		List<String> reduced = InputReader.readTestInput().stream()
+//				.filter(line -> line.contains("S") || line.contains("^")).toList();
+		
+		List<String> reduced = InputReader.readInput().stream()
+				.filter(line -> line.contains("S") || line.contains("^")).toList();
+		
+		Grid grid = Grid.parse(reduced, mappingConfig);
+		
+		Thread t = new Thread(new TachyonLab(grid));
+		t.start();
 	}
-	
 
 }
--- Day07/src/main/java/org/jksoft/aoc/TachyonLab.java
+++ Day07/src/main/java/org/jksoft/aoc/TachyonLab.java
@@ -4,53 +4,72 @@ import org.jksoft.utils.grid.Grid;
 import org.jksoft.utils.grid.Tile;
 import org.jksoft.utils.grid.TileType;
 
-public class TachyonLab {
+public class TachyonLab implements Runnable {
 	private Grid grid;
-	
-	private int splits;
-	
+
+	private int result;
+	private long[][] arr;
+
 	public TachyonLab(Grid grid) {
 		this.grid = grid;
-		this.splits = 0;
+		this.result = 0;
+
+		this.arr = new long[this.grid.getWidth()][this.grid.getHeight()];
 	}
-	
-	public void fire() {
+
+	@Override
+	public void run() {
 		System.out.println("firing up tachyon ray emitter");
 		System.out.println("heating-sequnece succesfull");
 		System.out.println("aperture closed for a concentrated beam");
 		System.out.println("critical beam focus detected");
 		System.out.println("shutter opened");
-		Tile tile = this.grid.findTile(TileType.STAR);
-		followRay(tile);
-		System.out.println(this.grid);
-	}
-	
-	private void followRay(Tile current) {
-		if(current.getY() >= this.grid.getHeight() - 1) {
+
+		Tile start = null;
+
+		for (int i = 0; i < this.grid.getWidth(); i++) {
+			Tile potTile = this.grid.getTile(i, 0);
+			if (potTile.getType().equals(TileType.STAR)) {
+				start = potTile;
+				break;
+			}
+		}
+
+		if (start == null)
 			return;
-		}
-		
-		Tile below = this.grid.getSouthernNeighbor(current);
-		if(below.getType().equals(TileType.DOT)) {
-			below.setType(TileType.PIPE);
-			followRay(below);
-		} else if(below.getType().equals(TileType.PLUS)){
-			this.splits++;
-			Tile left = this.grid.getWesternNeighbor(below);
-			if(left.getType().equals(TileType.DOT)) {
-				left.setType(TileType.PIPE);
-				followRay(left);
-			}
-			Tile right = this.grid.getEasternNeighbor(below);
-			if(right.getType().equals(TileType.DOT)) {
-				right.setType(TileType.PIPE);
-				followRay(right);
-			}
-		}
+
+		this.arr[start.getX()][start.getY()] = 1;
+		this.analyzeGrid(start);
 	}
-	
-	
+
+	private void analyzeGrid(Tile start) {
+		for (int y = 1; y < this.grid.getHeight(); y++) {
+			for (int x = 0; x < this.grid.getWidth(); x++) {
+				Tile t = this.grid.getTile(x, y);
+				if (TileType.PLUS.equals(t.getType()))
+					continue;
+				else {
+					long value = 0;
+					if (x > 1 && this.grid.getWesternNeighbor(t).getType().equals(TileType.PLUS))
+						value += this.arr[x - 1][y - 1];
+					if (x < this.grid.getWidth() - 1 && this.grid.getEasternNeighbor(t).getType().equals(TileType.PLUS))
+						value += this.arr[x + 1][y - 1];
+					if (this.grid.getNorthernNeighbor(t).getType().equals(TileType.DOT))
+						value += this.arr[x][y - 1];
+					this.arr[x][y] = value;
+				}
+			}
+		}
+
+		long result = 0;
+		for (int x = 0; x < this.grid.getWidth(); x++) {
+			result += this.arr[x][this.grid.getHeight() - 1];
+		}
+
+		System.out.println(result);
+	}
+
 	public int getResult() {
-		return this.splits;
+		return this.result;
 	}
 }
